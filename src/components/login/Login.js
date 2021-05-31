38c9a559c32d98f3fff290607f513aaae0c0eac1
Mohamed Zayan <m7mdzayan@gmail.com>
2021-06-01 00:11:42 +0200

    add user login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import "./login.css";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,12 +29,56 @@ const useStyles = makeStyles((theme) => ({
 const Login = () => {
   const classes = useStyles();
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, seterrorMessage] = useState("");
+  const [showErrorMessage, setshowErrorMessage] = useState(false);
+
+  const handleSubmit = async () => {
+    const loginData = {
+      email,
+      password,
+    };
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/api/login",
+        loginData
+      );
+      console.log(res.data);
+      //   localStorage.setItem("userToken", res.data.userToken);
+      //   localStorage.setItem("authenticated", true);
+      //   history.push("/");
+      //   console.log(res.data.userToken);
+    } catch (e) {
+      seterrorMessage(e.response.data.message);
+      setshowErrorMessage(true);
+    }
+  };
+
   return (
     <div className="form__container">
       <form className={classes.root} noValidate autoComplete="off">
-        <TextField label="email" variant="outlined" />
-        <TextField label="password" type="password" variant="outlined" />
-        <Button variant="contained" color="primary" className={classes.btn}>
+        {showErrorMessage ? <p className="form_error">{errorMessage}</p> : null}
+
+        <TextField
+          label="email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="password"
+          type="password"
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.btn}
+          onClick={handleSubmit}
+        >
           Login
         </Button>
       </form>
