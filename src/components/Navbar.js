622057fefd752053777295e459e54e1fa851a014
Mohamed Zayan <m7mdzayan@gmail.com>
2021-06-02 01:00:23 +0200

    add logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -7,6 +7,8 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
+import { userContext } from "../context/userContext";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,6 +28,13 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Navbar() {
   const classes = useStyles();
+  const [userData, setUserData] = useContext(userContext);
+  let history = useHistory();
+
+  const handleLogout = () => {
+    setUserData({ userToken: "", authenticated: false });
+    history.push("/");
+  };
 
   return (
     <div className={classes.root}>
@@ -44,12 +53,26 @@ export default function Navbar() {
               ECommerce App
             </Link>
           </Typography>
-          <Button variant="contained" className={classes.Button}>
-            <Link to="/login">Login</Link>
-          </Button>
-          <Button variant="contained">
-            <Link to="/signup">Sign up</Link>
-          </Button>
+
+          {userData.authenticated !== true ? (
+            <>
+              <Button variant="contained" className={classes.Button}>
+                <Link to="/login">Login</Link>
+              </Button>
+              <Button variant="contained">
+                <Link to="/signup">Sign up</Link>
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button variant="contained" className={classes.Button}>
+                <Link to="/login">Shopping Cart</Link>
+              </Button>
+              <Button variant="contained" onClick={handleLogout}>
+                Log out
+              </Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </div>
