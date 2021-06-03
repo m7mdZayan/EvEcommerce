7212778dae205172bc5f0902831e71469524dd5b
Mohamed Zayan <m7mdzayan@gmail.com>
2021-06-03 17:39:08 +0200

    increase decrease & remove product from cart done

--- src/containers/shoppingCart/ShoppingCart.js
+++ src/containers/shoppingCart/ShoppingCart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { userContext } from "../../context/userContext";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -17,27 +17,56 @@ const useStyles = makeStyles({
 });
 
 const ShoppingCart = () => {
-  const [userData] = useContext(userContext);
+  const [userData, setUserData] = useContext(userContext);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(userData.userCart);
+  }, [userData]);
 
   const classes = useStyles();
 
+  const increaseQuantity = (product) => {
+    product.amount++;
+    setUserData({ ...userData, userCart: products });
+  };
+
+  const decreaseQuantity = (product) => {
+    if (product.amount === 1) {
+      let newProducts = products.filter((pro) => pro._id !== product._id);
+      setProducts(newProducts);
+      console.log(products);
+      setUserData({ ...userData, userCart: newProducts });
+    } else {
+      product.amount--;
+      setUserData({ ...userData, userCart: products });
+    }
+    console.log(products);
+  };
+
   return (
     <div style={{ minHeight: "75vh", padding: " 20px 0 " }}>
-      {userData.userCart.length > 0 ? (
+      {products.length > 0 ? (
         <TableContainer component={Paper}>
           <Table className={classes.table} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell>Product Name</TableCell>
-                <TableCell>Product Description</TableCell>
-                <TableCell>Price</TableCell>
-                <TableCell>Quantity</TableCell>
-                <TableCell>+ / -</TableCell>
-                <TableCell>Total Price</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>
+                  Product Name
+                </TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>
+                  Product Description
+                </TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>Price</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>Quantity</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>+ / -</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>
+                  Total Price
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {userData.userCart.map((product) => (
+              {products.map((product) => (
                 <TableRow key={product._id}>
                   <TableCell component="th" scope="row">
                     {product.name}
@@ -46,21 +75,39 @@ const ShoppingCart = () => {
                   <TableCell>{product.price}</TableCell>
                   <TableCell>{product.amount}</TableCell>
                   <TableCell>
-                    <Button variant="contained" color="primary">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => increaseQuantity(product)}
+                      style={{ marginRight: "5px" }}
+                    >
                       +
                     </Button>
-                    <Button variant="contained" color="secondary">
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => decreaseQuantity(product)}
+                    >
                       -
                     </Button>
                   </TableCell>
-                  <TableCell> {300} EGP</TableCell>
+                  <TableCell> {product.price * product.amount} EGP</TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
       ) : (
-        <p>your cart is empty</p>
+        <p
+          style={{
+            textAlign: "center",
+            color: "#f00",
+            fontSize: "24px",
+            margin: "50px 0",
+          }}
+        >
+          your cart is empty
+        </p>
       )}
     </div>
   );
